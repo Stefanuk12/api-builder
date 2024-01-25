8c6215fde91a61e16ebca9904327f593c5b9ed1a
Stefan <42220813+Stefanuk12@users.noreply.github.com>
2024-01-25 14:14:11 +0000

    add helper map function

--- api-builder/src/error.rs
+++ api-builder/src/error.rs
@@ -54,3 +54,19 @@ pub enum APIError<E: std::error::Error + Send + Sync + 'static> {
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
+impl<E: std::error::Error + Send + Sync + 'static> APIError<E> {
+    pub fn map_err<T: std::error::Error + Send + Sync + 'static + Into<E>>(err: APIError<T>) -> APIError<E> {
+        match err {
+            APIError::Client(e) => APIError::Client(e.into()),
+            APIError::HTTP(e) => APIError::HTTP(e),
+            #[cfg(feature = "reqwest")]
+            APIError::Reqwest(e) => APIError::Reqwest(e),
+            APIError::Body(e) => APIError::Body(e),
+            APIError::Header(e) => APIError::Header(e),
+            APIError::MissingHeaderName => APIError::MissingHeaderName,
+            APIError::Response(e) => APIError::Response(e),
+            APIError::URL(e) => APIError::URL(e),
+            APIError::Other(e) => APIError::Other(e),
+        }
+    }
+}
